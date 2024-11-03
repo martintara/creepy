906fe4f86055fa1767f57718b64f20e33137296d
Martin Taraldstad <martin.taraldstad@gmail.com>
2024-11-03 14:47:56 +0100

    testing

--- src/leg.py
+++ src/leg.py
@@ -3,12 +3,10 @@ from servo import Servo
 from maestro import Controller
 
 class Leg:
-    def __init__(self, leg_id: int, servo_params, controller: Controller):
+    def __init__(self, leg_id: int, servo_params, controller: Controller, offset=0):
         self.leg_id = leg_id  # Unique identifier for the leg
-        self.offset = servo_params[0].get("offset", 0)  # Get offset from first servo
-        self.servos = [
-            Servo(controller, **params) for params in servo_params
-        ]
+        self.offset = offset
+        self.servos = [Servo(controller, **params) for params in servo_params]  # Initialize servos for the leg
     def lower_leg(self):
         # self.servo_0.move(1474) #commented out while testing
 #       self.servos[1].move(int(((((self.servos[1].max_pos+self.servos[1].min_pos)/2)+self.servos[1].max_pos))/2))
@@ -34,6 +32,18 @@ class Leg:
         self.servos[0].move(self.servos[0].center_pos)
         self.servos[1].move(self.servos[1].min_pos)
         self.servos[2].move(self.servos[2].min_pos)
+
+    def leg_forward(self):
+        """Move leg forward, adjusted for orientation."""
+        forward_position = self.servos[0].max_pos if self.offset in [0, 45, 90] else self.servos[0].min_pos
+        self.servos[0].move(forward_position)
+        print(f"Leg {self.leg_id} moved forward with orientation offset {self.offset}")
+
+    def leg_backward(self):
+        """Move leg backward, adjusted for orientation."""
+        backward_position = self.servos[0].min_pos if self.offset in [0, 45, 90] else self.servos[0].max_pos
+        self.servos[0].move(backward_position)
+        print(f"Leg {self.leg_id} moved backward with orientation offset {self.offset}")
 
     def manual_control(self, id: int):
         self.servos[id].manual_control()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # main.py
-
+import json
 from creepy_pod import CreepyPod
 from creepy_state import CreepyState
 from maestro import Controller
@@ -10,42 +10,67 @@ import time
 # from servo import Servo
 
 def main():
+    def load_leg_params(filename):
+        """Load leg parameters from a JSON file."""
+        with open(filename, 'r') as file:
+            leg_params = json.load(file)
+        return leg_params 
     # servo controller communication object
     ctrl = maestro.Controller()
-
+    leg_params = load_leg_params("default_leg_params.json")
     # Servo settings
-    leg_params = [
-        [  # Leg 0
-            {"channel": 0, "center_pos": 5900, "range": 2350, "center_angle": 0, "angle_range": 45, "offset": 315},
-            {"channel": 1, "center_pos": 6125, "range": 3775, "center_angle": 0, "angle_range": 45},
-            {"channel": 2, "center_pos": 5100, "range": 3000, "center_angle": 0, "angle_range": 70}
-        ],
-        [  # Leg 1
-            {"channel": 3, "center_pos": 6025, "range": 2350, "center_angle": 90, "angle_range": 90, "offset": 0},
-            {"channel": 4, "center_pos": 6400, "range": 3775, "center_angle": 90, "angle_range": 90},
-            {"channel": 5, "center_pos": 5125, "range": 3250, "center_angle": 90, "angle_range": 90}
-        ],
-        [  # Leg 2
-            {"channel": 6, "center_pos": 6400, "range": 2350, "center_angle": 90, "angle_range": 90, "offset": 45},
-            {"channel": 7, "center_pos": 6450, "range": 3775, "center_angle": 90, "angle_range": 90},
-            {"channel": 8, "center_pos": 5050, "range": 3250, "center_angle": 90, "angle_range": 90}
-        ],
-        [  # Leg 3
-            {"channel": 9, "center_pos": 5750, "range": 2350, "center_angle": 90, "angle_range": 90, "offset": 135},
-            {"channel": 10, "center_pos": 5875, "range": 3775, "center_angle": 90, "angle_range": 90},
-            {"channel": 11, "center_pos": 5200, "range": 3250, "center_angle": 90, "angle_range": 90}
-        ],
-        [  # Leg 4
-            {"channel": 12, "center_pos": 5600, "range": 2350, "center_angle": 90, "angle_range": 90, "offset": 180},
-            {"channel": 13, "center_pos": 6400, "range": 3775, "center_angle": 90, "angle_range": 90},
-            {"channel": 14, "center_pos": 5025, "range": 3250, "center_angle": 90, "angle_range": 90}
-        ],
-        [  # Leg 5
-            {"channel": 15, "center_pos": 5000, "range": 2350, "center_angle": 90, "angle_range": 90, "offset": 225},
-            {"channel": 16, "center_pos": 6250, "range": 3775, "center_angle": 90, "angle_range": 90},
-            {"channel": 17, "center_pos": 5025, "range": 3250, "center_angle": 90, "angle_range": 90}
-        ]
-    ]
+    # leg_params = [
+    #         {
+    #         "offset": 315,
+    #         "servos": [
+    #             {"channel": 0, "center_pos": 5900, "range": 2350, "center_angle": 0, "angle_range": 45},
+    #             {"channel": 1, "center_pos": 6125, "range": 3775, "center_angle": 0, "angle_range": 45},
+    #             {"channel": 2, "center_pos": 5100, "range": 3000, "center_angle": 0, "angle_range": 70}
+    #         ]
+    #         },
+    #         { 
+    #         "offset": 0,
+    #         "servos": [  # Leg 1
+    #             {"channel": 3, "center_pos": 6025, "range": 2350, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 4, "center_pos": 6400, "range": 3775, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 5, "center_pos": 5125, "range": 3250, "center_angle": 90, "angle_range": 90}
+    #         ]
+    #         },
+    #         { 
+    #         "offset": 45,
+    #         "servos": [  # Leg 2
+    #             {"channel": 6, "center_pos": 6400, "range": 2350, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 7, "center_pos": 6450, "range": 3775, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 8, "center_pos": 5050, "range": 3250, "center_angle": 90, "angle_range": 90}
+    #         ]
+    #         },
+    #         { 
+    #         "offset": 135,
+    #         "servos": [  # Leg 3
+    #             {"channel": 9, "center_pos": 5750, "range": 2350, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 10, "center_pos": 5875, "range": 3775, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 11, "center_pos": 5200, "range": 3250, "center_angle": 90, "angle_range": 90}
+    #         ]
+    #         },
+    #         { 
+    #         "offset": 180,
+    #         "servos": [  # Leg 4
+    #             {"channel": 12, "center_pos": 5600, "range": 2350, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 13, "center_pos": 6400, "range": 3775, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 14, "center_pos": 5025, "range": 3250, "center_angle": 90, "angle_range": 90}
+    #         ]
+    #         },
+    #         { 
+    #         "offset": 225,
+    #         "servos": [  # Leg 5
+    #             {"channel": 15, "center_pos": 5000, "range": 2350, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 16, "center_pos": 6250, "range": 3775, "center_angle": 90, "angle_range": 90},
+    #             {"channel": 17, "center_pos": 5025, "range": 3250, "center_angle": 90, "angle_range": 90}
+    #         ]
+    #         }
+    #     ]
+
+
     creepy_pod = CreepyPod(leg_params, ctrl)  # Initialize CreepyPod in STARTUP state
 
     # Run state actions until shutdown
