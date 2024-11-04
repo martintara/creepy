6b2521e535724c02e68090418477b0e0a533d4b7
Martin Taraldstad <martin.taraldstad@gmail.com>
2024-11-04 14:37:26 +0100

    sync

--- src/gait.py
+++ src/gait.py
@@ -9,20 +9,19 @@ class Gait:
         self.creepy_pod = creepy_pod
 
     def tripod_gait(self):
-        # Phase 1: Move legs 0, 3, and 4
         self.creepy_pod.legs[0].leg_forward()
-        self.creepy_pod.legs[1].leg_forward()
         self.creepy_pod.legs[2].leg_forward()
-        self.creepy_pod.legs[3].leg_forward()
         self.creepy_pod.legs[4].leg_forward()
-        self.creepy_pod.legs[5].leg_forward()
+        self.creepy_pod.legs[1].leg_backward()
+        self.creepy_pod.legs[3].leg_backward()
+        self.creepy_pod.legs[5].leg_backward()
         time.sleep(2)
         self.creepy_pod.legs[0].leg_backward()
-        self.creepy_pod.legs[1].leg_backward()
         self.creepy_pod.legs[2].leg_backward()
-        self.creepy_pod.legs[3].leg_backward()
         self.creepy_pod.legs[4].leg_backward()
-        self.creepy_pod.legs[5].leg_backward()
+        self.creepy_pod.legs[1].leg_forward()
+        self.creepy_pod.legs[3].leg_forward()
+        self.creepy_pod.legs[5].leg_forward()
         time.sleep(2)
 
     def wave_gait(self):
