1ee3a86d37c73988f3ca4389489554c9cbe6076f
Didrik <didrikber@gmail.com>
2024-11-25 11:40:53 +0100

    changed spider image in display.py

--- src/display.py
+++ src/display.py
@@ -13,22 +13,14 @@ red = [255, 0, 0]
 
 #Define images and animations
 spider_image =[
-    red, white, white, white, white, white, white, red,
-    white, red, white, white, white, white, red, white,
-    white, white, red, red, red, red, white, white,
-    red, white, red, red, red, red, white, red,
-    white, red, red, red, red, red, red, white,
-    white, white, red, red, red, red, white, white,
-    white, red, white, white, white, white, red, white,
-    red, white, white, white, white, white, white, red
-#red, white, white, white, white, white, white, red,
-# white, red, red, white, white, red, red, white,
-# white, white, white, red, red,  white, white, white,
-#red, red, red, red, red, red, red, red,
-# white, white, white, red, red,  white, white, white,
-# white, red, red, red, red, red, red,  white,
-#red, white, white,blue, blue,  white, white, red,
-#red,  white, white, white, white, white, white, red
+red, white, white, white, white, white, white, red,
+ white, red, red, white, white, red, red, white,
+ white, white, white, red, red,  white, white, white,
+red, red, red, red, red, red, red, red,
+ white, white, white, red, red,  white, white, white,
+ white, red, red, red, red, red, red,  white,
+red, white, white,blue, blue,  white, white, red,
+red,  white, white, white, white, white, white, red
 ]
 
 def idle():
